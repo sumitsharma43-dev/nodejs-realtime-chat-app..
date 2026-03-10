5e42061d355926f5ac7aac5209032ecd1ac4a405
Sumi Sharma <sumitsharma43120@gmail.com>
2026-03-11 03:58:16 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4,17 +4,35 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const messages = document.getElementById("messages");
 
+const usernameInput = document.getElementById("username");
+const joinBtn = document.getElementById("joinBtn");
+
+let username = "";
+
+joinBtn.addEventListener("click", () => {
+  username = usernameInput.value;
+
+  if (username.trim() !== "") {
+    document.getElementById("username-container").style.display = "none";
+    form.style.display = "flex";
+  }
+});
+
 form.addEventListener("submit", function(e) {
   e.preventDefault();
 
   if (input.value) {
-    socket.emit("chat message", input.value);
+    socket.emit("chat message", {
+      user: username,
+      text: input.value
+    });
+
     input.value = "";
   }
 });
 
 socket.on("chat message", function(msg) {
   const item = document.createElement("li");
-  item.textContent = msg;
+  item.textContent = msg.user + ": " + msg.text;
   messages.appendChild(item);
 });
